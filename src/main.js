41b322504f8f42c6b33bff3ac80a48a91b797b5a
Leandro Henrique Silva Lima <leandrohenriquesilvalima@gmail.com>
2025-11-01 12:32:19 -0600

    correcao para acessar o dashboard

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import App from './App.vue'
 import router from './router'
 import { createApp } from 'vue'
+import '@/scss/style.scss';
+import vuetify from './plugins/vuetify'
 const app = createApp(App)
 app
   .use(router)
+  .use(vuetify)
   .mount('#app')
