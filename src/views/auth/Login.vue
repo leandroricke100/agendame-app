15c21ae76566e1be824c5c5956aca41e3ee26737
Leandro Henrique Silva Lima <leandrohenriquesilvalima@gmail.com>
2025-11-04 17:23:12 -0700

    correcao axios

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -8,9 +8,12 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 
-axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie')
+axios.get('http://localhost:8000/sanctum/csrf-cookie')
   .then(() => {
-    axios.post('http://127.0.0.1:8000/api/login')
+    axios.post('http://localhost:8000/api/login', {
+      email: 'test@example.com',
+      password: 'password'
+    })
   });
 
 
