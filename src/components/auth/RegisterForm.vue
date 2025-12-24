3d406029e7a9b99d7499ff6775065b7dd7fc73fc
Leandro Henrique Silva Lima <leandrohenriquesilvalima@gmail.com>
2025-12-24 10:36:30 -0300

    att pagina de cadastro

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,24 +1,36 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-const checkbox = ref(true);
-</script>
-
 <template>
-    <v-row class="d-flex mb-3">
-        <v-col cols="12">
-            <v-label class="font-weight-bold mb-1">Name</v-label>
-            <v-text-field variant="outlined" hide-details color="primary"></v-text-field>
-        </v-col>
-        <v-col cols="12">
-            <v-label class="font-weight-bold mb-1">Email Address</v-label>
-            <v-text-field variant="outlined" type="email" hide-details color="primary"></v-text-field>
-        </v-col>
-        <v-col cols="12">
-            <v-label class="font-weight-bold mb-1">Password</v-label>
-            <v-text-field variant="outlined" type="password"  hide-details color="primary"></v-text-field>
-        </v-col>
-        <v-col cols="12" >
-            <v-btn to="/" color="primary" size="large" block   flat>Sign up</v-btn>
-        </v-col>
-    </v-row>
+  <v-row class="d-flex mb-3">
+    <v-col cols="12">
+      <v-label class="font-weight-bold mb-1">Nome</v-label>
+      <v-text-field variant="outlined" hide-details color="primary"></v-text-field>
+    </v-col>
+    <v-col cols="12">
+      <v-label class="font-weight-bold mb-1">E-mail</v-label>
+      <v-text-field variant="outlined" type="email" hide-details color="primary"></v-text-field>
+    </v-col>
+    <v-col cols="12">
+      <v-label class="font-weight-bold mb-1">Senha</v-label>
+      <v-text-field variant="outlined" type="password" hide-details color="primary"></v-text-field>
+    </v-col>
+    <v-col cols="12">
+      <v-btn color="primary" size="large" block flat @click="register">Cadastrar</v-btn>
+    </v-col>
+  </v-row>
 </template>
+
+<script setup>
+
+
+
+// import axios from 'axios';
+
+// function register() {
+//   const data = {
+//     first_name: 'test',
+//     email: 'test3@test.com',
+//     password: '12345678A'
+//   }
+
+//   axios.post('api/register', data)
+// }
+</script>
